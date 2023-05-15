084fa5528a90a2ce688c37993e2a2a7fe0be5cac
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2023-05-15 12:01:06 -0300

    Chat
    implement and chat func

--- Metaverso/Assets/MultiPlayerChat.cs
+++ Metaverso/Assets/MultiPlayerChat.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Fusion;
 
-public class Chat : MonoBehaviour
+public class MultiPlayerChat : NetworkBehaviour
 {
-   
+    public Text _message;
+    public Text input;
+    public TextMeshProUGUI usernameInput;
+    public string username = "Default";
+    
     public GameObject chatPanel;
+    
     private bool painelChat = false;
+
+    private void Start()
+    {
+        username = usernameInput.text;
+    }
 
     private void Update()
     {
@@ -20,6 +33,7 @@ public class Chat : MonoBehaviour
             Debug.Log("===== click ======");
         }
     }
+    //funcao de habilitar e desabilitar o chat no canvas
     public void ClickButtonChat()
     {
         painelChat = !painelChat;
@@ -34,5 +48,19 @@ public class Chat : MonoBehaviour
             chatPanel.SetActive(false);
             Debug.Log("======== inative==========");
         }
+    }
+
+    public void CallMessageRPC()
+    {
+        string message = input.text;
+        RPC_SendMessage(username, message);
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+
+    public void RPC_SendMessage(string username, string message, RpcInfo rpcInfo = default)
+    {
+
+        _message.text += $"{username}: {message}\n";
     }
 }
