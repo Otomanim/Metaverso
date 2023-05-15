8ed8415a4757e6a3849bca0dbec0b361f0ae4416
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2023-05-15 09:32:20 -0300

    Create menu

---
+++ Metaverso/Assets/Microfone.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Microfone : MonoBehaviour
+{
+    public GameObject speaker;
+    public GameObject microfone;
+    private AudioSource audioSource;
+    private bool isMuted = false;
+
+    private void Start()
+    {
+        audioSource = speaker.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ClickButton();
+        }
+    }
+
+    public void ClickButton()
+    {
+        isMuted = !isMuted;
+        if (isMuted)
+        {
+            Debug.Log("mute on");
+            audioSource.mute = true;
+            //microfone.GetComponents<IMa>
+
+        }
+        else
+        {
+            Debug.Log("mute off");
+            audioSource.mute = false;
+            //audioSource.clip = Microphone.Start(null, true, 10, AudioSettings.outputSampleRate);
+            //audioSource.loop = true;
+            //while (!(Microphone.GetPosition(null) > 0)) { }
+            //audioSource.Play();
+        }
+    }
+}
